dc872b3c7160e49922f9b46ecafbf24646fcfe21
Kane Mathis <admin@kanemathis.com>
2022-06-14 19:32:01 -0400

    this works

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,22 @@
 import React from "react";
+import Links from "./Links";
 
-function About() {
-  return (
+function About(props) {
+  if (!props.bio){
+    return (
+      <div id="about">
+      <h2>About Me</h2>
+      <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
+      <Links github="https://github.com/KDev-inAugust/react-hooks-props-basics-lab" linkedin="https://www.linkedin.com/feed/" />
+    </div>
+    )
+  }
+  else return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      <p>{props.bio}</p>
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      <Links github="https://github.com/KDev-inAugust/react-hooks-props-basics-lab" linkedin="https://www.linkedin.com/feed/"/>
     </div>
   );
 }
--- src/components/App.js
+++ src/components/App.js
@@ -11,8 +11,8 @@ function App() {
   return (
     <div>
       <NavBar />
-      <Home />
-      <About />
+      <Home name="Kane" city="New York" color="red"/>
+      <About bio="I like to make things work"/>
     </div>
   );
 }
